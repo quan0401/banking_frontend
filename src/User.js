866cd2904168e91f62c008690a28db2e1d45a9ff
Ngo Tran Quang Sang <ngosangnt@gmail.com>
2024-05-16 17:45:54 +0700

    Update User.css and User.js
    Update style of User.css and change the structure of User.js

--- src/User.js
+++ src/User.js
@@ -29,44 +29,56 @@ const User = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="basic-information">
+    <div class="container">
+      <div class="text">
         <h1>Welcome, {Data.user.username}</h1>
-        <h2>Your balence now : {Data.user.balance}</h2>
       </div>
-
-      <div className="mb-3">
-        <input
-          type="text"
-          placeholder="Enter money : "
-          value={savingMoney}
-          onChange={(e) => setSavingMoney(e.target.value)}
-          className="form-control"
-        />
-      </div>
-
-      <div className="savingOption">
-        <select
-          name="savingOption"
-          value={option}
-          onChange={(e) => setOption(e.target.value)}
-        >
-          <option value="KH0">No limit time</option>
-          <option value="KH3">3 months</option>
-          <option value="KH6">6 months</option>
-        </select>
-      </div>
-
-      <p>Home address: {Data.user.homeAddress}</p>
-      <p>CCCD: {Data.user.cccd}</p>
-
-      <div>
-        {/* Summit button */}
-        <form onSubmit={handleSubmit}>
-          <button type="submit">Submit</button>
+      <div id="contact-form">
+        <div class="form-row">
+          <div class="input-data">
+            <h3>Your balence now : {Data.user.balance}</h3>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="input-data">
+            <input
+              type="text"
+              placeholder="Enter money : "
+              value={savingMoney}
+              onChange={(e) => setSavingMoney(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <div class="input-data">
+            <div className="savingOption">
+            <select
+              name="savingOption"
+              value={option}
+              onChange={(e) => setOption(e.target.value)}
+            >
+              <option value="KH0">No limit time</option>
+              <option value="KH3">3 months</option>
+              <option value="KH6">6 months</option>
+            </select>
+            </div>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="input-data text">
+          <p>Home address: {Data.user.homeAddress}</p>
+          <p>CCCD: {Data.user.cccd}</p>
+          </div>
+        </div>
+        <form class="form-row submit-btn" onSubmit={handleSubmit}>
+          <div class="input-data">
+            <div className="inner">
+              <button type="submit">Submit</button>
+            </div>
+          </div>
         </form>
       </div>
-    </div>
+  </div>
+   
   );
 };
 
