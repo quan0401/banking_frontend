dfdaeae7692bf8d196e88c6e5422655953a827da
HiamSonn <dangthanhson2016@gmail.com>
2024-05-23 11:31:57 +0700

    FE UserInfo screen

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -4,8 +4,16 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
 import userDataTest from "./userData.json";
+import iconPencil from "./icons/pencil.png";
+import iconId from "./icons/id.png";
+import iconBalance from "./icons/money.png";
+import iconPhone from "./icons/phone.png";
+import iconEmail from "./icons/mail.png";
+import iconCCCD from "./icons/cccd.png";
+import iconHome from "./icons/home.png";
+import iconCreated from "./icons/add-user.png";
 
-const UserInfo = () => {
+function UserInfo() {
   const [userData, setUserData] = useState({});
   const [loading, setLoading] = useState(true);
   const dateTime = new Date(userDataTest.user.createdAt).toLocaleDateString();
@@ -29,145 +37,65 @@ const UserInfo = () => {
   }
 
   return (
-    <>
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>User Information</title>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-        />
-      </head>
-      <body style={{ backgroundColor: "#353353" }}>
-        <div class="container">
-          <h1 style={{ color: "white" }}>User Information</h1>
-
-          <div id="user-info" class="card mt-3">
-            <div class="card-header">User Details</div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <img
-                      src={userDataTest.user.profilePicture}
-                      alt="Profile Picture"
-                      id="profilePicture"
-                      class="img-thumbnail"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="id">ID:</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="id"
-                      readonly
-                      disabled
-                      value={userDataTest.user.id}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="username">Username:</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="username"
-                      readonly
-                      disabled
-                      value={userDataTest.user.username}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="profilePublicID">Profile Public ID:</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="profilePublicID"
-                      readonly
-                      disabled
-                      value={userDataTest.user.profilePublicId}
-                    />
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <label for="balance">Balance:</label>
-                    <input
-                      type="number"
-                      class="form-control"
-                      id="balance"
-                      readonly
-                      disabled
-                      value={userDataTest.user.balance}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="phone">Phone Number:</label>
-                    <input
-                      type="tel"
-                      class="form-control"
-                      id="phone"
-                      readonly
-                      disabled
-                      value={userDataTest.user.phone}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="email"
-                      readonly
-                      disabled
-                      value={userDataTest.user.email}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="cccd">CCCD:</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="cccd"
-                      readonly
-                      disabled
-                      value={userDataTest.user.cccd}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="address">Address:</label>
-                    <textarea
-                      class="form-control"
-                      id="address"
-                      rows="3"
-                      readonly
-                      disabled
-                      value={userDataTest.user.homeAddress}
-                    ></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="createAt">Created At:</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="createAt"
-                      readonly
-                      disabled
-                      value={dateTime}
-                    />
-                  </div>
-                </div>
-              </div>
+    <div className="desktop">
+      <div className="left-column">
+        <div className="avatar">
+          <img
+            className="mask-group"
+            alt="Mask group"
+            src={userDataTest.user.profilePicture}
+          />
+        </div>
+        <button className="overlap-group">
+          <img className="pencil-icon" alt="Pencil icon" src={iconPencil} />
+          <div className="text-wrapper">Sửa</div>
+        </button>
+      </div>
+      <div className="right-column">
+        <div className="text-wrapper-2">{userDataTest.user.username}</div>
+        <div className="info-section">
+          <div className="info-item">
+            <img className="icon" alt="Id icon" src={iconId} />
+            <div className="text-wrapper-4">ID : {userDataTest.user.id}</div>
+          </div>
+          <div className="info-item">
+            <img className="icon" alt="Balance icon" src={iconBalance} />
+            <div className="text-wrapper-5">
+              Balance : {userDataTest.user.balance}
             </div>
           </div>
+          <div className="info-item">
+            <img className="icon" alt="Phone icon" src={iconPhone} />
+            <div className="text-wrapper-6">
+              Phone number : {userDataTest.user.phone}
+            </div>
+          </div>
+          <div className="info-item">
+            <img className="icon" alt="Mail icon" src={iconEmail} />
+            <div className="text-wrapper-7">
+              Email address : {userDataTest.user.email}
+            </div>
+          </div>
+          <div className="info-item">
+            <img className="icon" alt="Cccd icon" src={iconCCCD} />
+            <div className="text-wrapper-8">
+              CCCD : {userDataTest.user.cccd}
+            </div>
+          </div>
+          <div className="info-item">
+            <img className="icon" alt="Home icon" src={iconHome} />
+            <div className="text-wrapper-3">
+              Home Address : {userDataTest.user.homeAddress}
+            </div>
+          </div>
+          <div className="info-item">
+            <img className="icon" alt="Created icon" src={iconCreated} />
+            <div className="text-wrapper-9">Created at : {dateTime}</div>
+          </div>
         </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="script.js"></script>
-      </body>
-    </>
+      </div>
+    </div>
   );
-};
+}
 
 export default UserInfo;
