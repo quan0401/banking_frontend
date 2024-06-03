645ee32e77c59e37a7370365de2fbccacd91a37c
SonRobo <22521254@gm.uit.edu.vn>
2024-06-03 14:52:59 +0700

    resolve login error

--- src/Login.js
+++ src/Login.js
@@ -1,42 +1,51 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import Validation from './SignUpValidation';
+import Validation from "./SignUpValidation";
 
 const Login = () => {
   // login
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   //signup
-  const [username, setUsername] = useState('');
-  const [country, setCountry] = useState('');
-  const [phone, setPhone] = useState('');
-  const [profilePicture, setProfilePicture] = useState('');
+  const [username, setUsername] = useState("");
+  const [country, setCountry] = useState("");
+  const [phone, setPhone] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
   const [signupSuccess, setSignupSuccess] = useState(false); // State to track signup success
   const [signupError, setSignupError] = useState(false); // State to store signup error message
 
   const navigate = useNavigate();
 
-
-  const handleLogin = async () => { 
-    try { 
-      const response = await axios.post('http://localhost:6969/api/v1/signin', { email, password });
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:6969/api/v1/signin", {
+        email,
+        password,
+      });
       const userData = response.data;
-      localStorage.setItem('userData', JSON.stringify(userData));
-      navigate('/home');
+      localStorage.setItem("userData", JSON.stringify(userData));
+      navigate("/home");
     } catch (error) {
-      console.error('Error during login:', error);
-      setError('Invalid email or password');
-    } 
-        // navigate('/home');
+      console.error("Error during login:", error);
+      setError("Invalid email or password");
+    }
+    // navigate('/home');
   };
 
   const handleSignup = async () => {
-    const validationErrors = Validation({ username, email, password, country, phone, profilePicture });
+    const validationErrors = Validation({
+      username,
+      email,
+      password,
+      country,
+      phone,
+      profilePicture,
+    });
     if (Object.keys(validationErrors).length !== 0) {
       setError(validationErrors);
       return;
@@ -57,11 +66,14 @@ const Login = () => {
           email,
           country,
           phone,
-          profilePicture: base64Data
+          profilePicture: base64Data,
         };
-  
-        const response = await axios.post("http://localhost:6969/api/v1/signup", userData);
-  
+
+        const response = await axios.post(
+          "http://localhost:6969/api/v1/signup",
+          userData
+        );
+
         setSignupSuccess(true); // Set signup success
         setError({}); // Clear any previous errors
         setSignupError(false); // Clear signup error
@@ -73,7 +85,7 @@ const Login = () => {
       console.error(error);
       setSignupError(true); // Set signup error
       setSignupSuccess(false); // Set signup success to false
-      
+
       // handle error, show error message
     }
   };
@@ -83,115 +95,142 @@ const Login = () => {
     setProfilePicture(file);
   };
 
-  // Change the screen 
-  // const signUpButton = document.getElementById('signUp');
-  // const signInButton = document.getElementById('signIn');
-  // const container = document.getElementById('container');
+  // Change the screen
+  useEffect(() => {
+    const signUpButton = document.getElementById("signUp");
+    const signInButton = document.getElementById("signIn");
+    const container = document.getElementById("container");
 
-  // signUpButton.addEventListener('click', () => {
-  //   container.classList.add('right-panel-active');
-  // });
+    if (signUpButton && signInButton && container) {
+      signUpButton.addEventListener("click", () => {
+        container.classList.add("right-panel-active");
+      });
 
-  // signInButton.addEventListener('click', () => {
-  //   container.classList.remove('right-panel-active');
-  // });
+      signInButton.addEventListener("click", () => {
+        container.classList.remove("right-panel-active");
+      });
+    }
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      if (signUpButton && signInButton) {
+        signUpButton.removeEventListener("click", () => {
+          container.classList.add("right-panel-active");
+        });
+
+        signInButton.removeEventListener("click", () => {
+          container.classList.remove("right-panel-active");
+        });
+      }
+    };
+  }, []);
 
   return (
     <div className="container" id="container">
-        <div className="form-container sign-up-container">
-            <form action="#">
-                <h1>Create Account</h1> 
-                <input
-                  type='text'
-                  placeholder='Enter Username'
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  className="form-control"
-                />
-                {error.name && <p className="text-danger">{error.name}</p>}
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Create Account</h1>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="form-control"
+          />
+          {error.name && <p className="text-danger">{error.name}</p>}
 
-                <input
-                  type='email'
-                  placeholder='Enter Email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="form-control"
-                />
-                {error.email && <p className="text-danger">{error.email}</p>}
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+          {error.email && <p className="text-danger">{error.email}</p>}
 
-                <input
-                  type='password'
-                  placeholder='Enter Password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="form-control"
-                />
-                {error.password && <p className="text-danger">{error.password}</p>}
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+          />
+          {error.password && <p className="text-danger">{error.password}</p>}
 
-                <input
-                  type='text'
-                  placeholder='Enter Country'
-                  value={country}
-                  onChange={(e) => setCountry(e.target.value)}
-                  className="form-control"
-                />
-                {error.country && <p className="text-danger">{error.country}</p>}
+          <input
+            type="text"
+            placeholder="Enter Country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            className="form-control"
+          />
+          {error.country && <p className="text-danger">{error.country}</p>}
 
-                <input
-                  type='tel'
-                  placeholder='Enter Phone'
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  className="form-control"
-                />
-                {error.phone && <p className="text-danger">{error.phone}</p>}
+          <input
+            type="tel"
+            placeholder="Enter Phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            className="form-control"
+          />
+          {error.phone && <p className="text-danger">{error.phone}</p>}
 
-                <input
-                  type='file'
-                  onChange={handleImageChange}
-                  className="form-control-file"
-                />
-                {error.profilePicture && <p className="text-danger">{error.profilePicture}</p>}
+          <input
+            type="file"
+            onChange={handleImageChange}
+            className="form-control-file"
+          />
+          {error.profilePicture && (
+            <p className="text-danger">{error.profilePicture}</p>
+          )}
 
-                <button  onClick={handleSignup}>Sign Up</button>
-            </form>
+          <button onClick={handleSignup}>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign in</h1>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+          />
+
+          <button onClick={handleLogin}>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button className="ghost" id="signIn">
+              Sign In
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>
+              Enter your personal details and start wonderful journey with us
+            </p>
+            <button className="ghost" id="signUp">
+              Sign Up
+            </button>
+          </div>
         </div>
-        <div className="form-container sign-in-container">
-            <form action="#">
-                <h1>Sign in</h1>
-                <input
-                  type='email'
-                  placeholder='Enter Email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="form-control"
-                />
-
-                <input
-                  type='password'
-                  placeholder='Enter Password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="form-control"
-                />
-
-                <button onClick={handleLogin}>Sign In</button>
-            </form>
-        </div>
-        <div className="overlay-container">
-            <div className="overlay">
-                <div className="overlay-panel overlay-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
-                    <button className="ghost" id="signIn">Sign In</button>
-                </div>
-                <div className="overlay-panel overlay-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Enter your personal details and start wonderful journey with us</p>
-                    <button className="ghost" id="signUp">Sign Up</button>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
   );
 };
--- src/User.js
+++ src/User.js
@@ -29,24 +29,39 @@ const User = () => {
   };
 
   return (
-    <div className="content">
-    
-      <div className="container">
-        <div className="row align-items-stretch no-gutters contact-wrap">
+    <div className="user-content">
+      <div className="user-container">
+        <div className="user-row align-items-stretch no-gutters user-contact-wrap">
           <div className="col-md-12">
-            <div className="form h-100">
+            <div className="user-form h-100">
               <h3>Happy to see you, {Data.user.username}</h3>
               <h4>Your balance now : {Data.user.balance}</h4>
-              <form className="mb-5" method="post" id="contactForm" name="contactForm">
+              <form
+                className="mb-5"
+                method="post"
+                id="contactForm"
+                name="contactForm"
+              >
                 <div className="row">
-                  <div classNameName="col-md-6 form-group mb-3">
-                    <label for="" className="col-form-label">Saving Plan Name *</label>
-                    <input type="text" className="form-control" name="name" id="name" placeholder="Your saving plan name" />
+                  <div className="col-md-6 form-group mb-3">
+                    <label className="user-col-form-label">
+                      Saving Plan Name *
+                    </label>
+                    <input
+                      type="text"
+                      className="user-form-control"
+                      name="name"
+                      id="name"
+                      placeholder="Your saving plan name"
+                    />
                   </div>
                   <div className="col-md-6 form-group mb-3">
-                    <label for="" className="col-form-label">Your Option *</label>
+                    <label className="user-col-form-label">Your Option *</label>
                     <select
-                      type="text" className="form-control" name="savingOption" id="savingOption"  
+                      type="text"
+                      className="user-form-control"
+                      name="savingOption"
+                      id="savingOption"
                       value={option}
                       onChange={(e) => setOption(e.target.value)}
                     >
@@ -60,37 +75,40 @@ const User = () => {
 
                 <div className="row">
                   <div className="col-md-12 form-group mb-3">
-                    <label for="budget" className="col-form-label">Your saving money</label>
+                    <label for="budget" className="user-col-form-label">
+                      Your saving money
+                    </label>
                     <input
                       type="text"
                       placeholder="Enter money : "
                       value={savingMoney}
                       onChange={(e) => setSavingMoney(e.target.value)}
-                      className="form-control"
+                      className="user-form-control"
                     />
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="col-md-12 form-group mb-3">
-                    <span>
-                      Your home addess : {Data.user.homeAddress}
-                    </span>
+                    <span>Your home address : {Data.user.homeAddress}</span>
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="col-md-12 form-group mb-3">
-                    <span>
-                      Your CCCD : {Data.user.cccd}
-                    </span>
+                    <span>Your CCCD : {Data.user.cccd}</span>
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="col-md-12 form-group">
-                    <input type="submit" value="Summit" className="btn btn-primary rounded-0 py-2 px-4" onSubmit={handleSubmit} />
-                    <span className="submitting"></span>
+                    <input
+                      type="submit"
+                      value="Submit"
+                      className="user-btn user-btn-primary rounded-0 py-2 px-4"
+                      onSubmit={handleSubmit}
+                    />
+                    <span className="user-submitting"></span>
                   </div>
                 </div>
               </form>
@@ -98,11 +116,9 @@ const User = () => {
           </div>
         </div>
       </div>
-
     </div>
-
-   
   );
 };
 
 export default User;
+
